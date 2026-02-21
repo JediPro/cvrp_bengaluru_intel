76a8951c5591cd8f56a528e27f63f1d2e478e2d1
Pratik C <pratikranit@gmail.com>
2026-02-21 16:26:18 +0530

    Sfnetwork processing part 4

--- cvrp_bengaluru_intel.R
+++ cvrp_bengaluru_intel.R
@@ -162,6 +162,7 @@ data_stop_popn <- read_rds(file = "data_stop_popn.rds") %>%
 # Iterate till limits reached
 
 # Process Road Network ---------------------------------------
+t0 <- Sys.time()
 sfnet_road <- sf_city_road %>%
   # Convert all to Multi line string, to enable later conversion to Linestring
   st_cast(to = "MULTILINESTRING") %>% 
@@ -224,26 +225,35 @@ sfnet_road <- sf_city_road %>%
   # Arrange in ascending order 
   arrange(edge_length()) %>% 
   filter(!(is.na(edge_is_loop()) | is.na(edge_is_multiple())))
+difftime(time1 = Sys.time(), time2 = t0)
 
+# Function to calculate clusters for any SF point object ----------------------
 fx_dbscan <- function(sf_points){
-  sf_points <- sfnet_road %>% st_as_sf("nodes")
+  # sf_points <- sfnet_road %>% st_as_sf("nodes")
     
   # Extract coordinates
   mtx_coords <- sf_points %>% 
     # Transform to mollweide coordinates to be able to set parameters in meters
     st_transform(crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m") %>% 
     # Fetch coordinate matrix
-    st_coordinates(x = sf_points)
+    st_coordinates()
   
-  # Number of points is pretty large, so need to run frNN before clustering
-  fr_neighbour <- dbscan::frNN(x = mtx_coords, eps = 25)
   # Fetch clusters
-  vec_cluster <- dbscan::dbscan(x = mtx_coords, eps = 25, minPts = 1)
+  vec_cluster <- dbscan::dbscan(x = mtx_coords, eps = 25, minPts = 1)$cluster
+  
+  # Bind to object
+  return(sf_points %>% mutate(cluster = vec_cluster))
 }
 
+# Contract network based on defined clusters --------------------------
 fgh <- sfnet_road %>% 
   # Contract network by replacing clustered notes with centroids
   activate(nodes) %>% 
-  (function(df) {
-    
-  })
+  # Apply function
+  fx_dbscan() %>% 
+  # Find the components again, so that only points in the same network are amalgamated
+  mutate(component = group_components()) %>% 
+  # Contract network
+  convert(.f = to_spatial_contracted, cluster, component,
+          simplify = TRUE, .clean = TRUE, 
+          summarise_attributes = list(edge_time = "sum", "ignore"))
